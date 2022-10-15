8eec584fecabbc10f74639b6acf9b231694e53b5
Aleksandr <dentranscom@gmail.com>
2022-10-15 22:31:05 +0300

    Add Enviroment Vars

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 //jshint esversion:6
+require('dotenv').config()
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
  
 const app = express();
  
@@ -10,6 +13,18 @@ app.set('view engine','ejs')
 app.use(bodyParser.urlencoded({
   extended:true
 }));
+
+mongoose.connect("mongodb://localhost:27017/userDB");
+
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String
+});
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+
+
+const User = new mongoose.model("User", userSchema);
  
 app.get("/", (req, res) => {
     res.render("home");
@@ -23,7 +38,36 @@ app.get("/register", (req, res) => {
     res.render("register");
 });
 
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
 
+app.post("/login", (req, res) => {
+  const userName = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email: userName}, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        if (foundUser.password === password) {
+          res.render("secrets");
+        }
+      }
+    }
+  });
+});
 
 
 app.listen(3000,function(req,res){
